1a81d351b2a90c1ed840f96f34f2be8bed308bdf
Johnny Li <xiao.li@ucdconnect.ie>
2020-05-12 00:19:49 +0100

    Update Link_to_Tweet.py

--- Project_CC/Link_to_Tweet.py
+++ Project_CC/Link_to_Tweet.py
@@ -4,10 +4,10 @@ import threading, time
 from datetime import datetime
 
 # Keys
-consumer_key = "DOuCN8WDK3wn2t4jZURScYldL"
-consumer_secret = "mrxK51iBqOubaeRvlRlTmPTpDCdP5YiPmqvO7WLc7FISHPuH0l"
-access_token = "1241367764750733313-AxuQhY63z0rtdGgrmDme6bLLiKH3ak"
-access_token_key = "mxMqCnrNcIIuOBm5US59LYnC2jTYtjROe9MXF6FrxfjAY"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_key = ""
 
 # Authorisation
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
